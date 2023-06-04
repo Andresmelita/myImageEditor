9a513ee8dff9f2a8dc1f532cd632849b0ab1117a
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-04 01:29:31 -0400

    feat: dropzone deleted of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RemoveBackGroundComponent } from './components/remove-back-ground/remove-back-ground.component';
@@ -12,7 +11,7 @@ import { RemoveBackGroundComponent } from './components/remove-back-ground/remov
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
