a26655be8a1e735f045228354d754d159b5b6693
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-04 17:47:42 -0400

    feat: deleted imports not used

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,10 @@
 import {NgModule} from '@angular/core'
-import { MatButtonToggleModule, MatButtonToggle } from '@angular/material/button-toggle'
-import { ButtonToggleComponent } from './components/button-toggle/button-toggle.component'
 
 
 @NgModule({
     imports: [
-        MatButtonToggleModule, 
-        ButtonToggleComponent
     ],
     exports: [
-        MatButtonToggleModule,
-        ButtonToggleComponent
     ]
 })
 export class MaterialModule {}
