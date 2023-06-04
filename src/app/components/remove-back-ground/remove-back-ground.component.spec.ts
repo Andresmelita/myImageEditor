da05c89b315665e53b7567fb05cd1508ca9c500d
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-03 22:32:27 -0400

    feat: new component added

--- src/app/components/remove-back-ground/remove-back-ground.component.spec.ts
+++ src/app/components/remove-back-ground/remove-back-ground.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RemoveBackGroundComponent } from './remove-back-ground.component';
 
 describe('RemoveBackGroundComponent', () => {
--- src/app/components/remove-back-ground/remove-back-ground.component.ts
+++ src/app/components/remove-back-ground/remove-back-ground.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-remove-back-ground',
   templateUrl: './remove-back-ground.component.html',
-  styleUrls: ['./remove-back-ground.component.scss']
 })
 export class RemoveBackGroundComponent {
 
