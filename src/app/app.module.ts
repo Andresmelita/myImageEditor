5631fd1b0ceb2f5b6cc7e66802c4d6954ba2e6e9
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-04 07:22:55 -0400

    feat: new imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RemoveBackGroundComponent } from './components/remove-back-ground/remove-back-ground.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import {CloudinaryModule} from '@cloudinary/ng';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RemoveBackGroundComponent
+    RemoveBackGroundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    CloudinaryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
