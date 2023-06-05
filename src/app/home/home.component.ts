65cd100a2d182b91ebd16be1e509995d02289116
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-05 18:51:19 -0400

    feat: animation dependencies added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { RemoveBackGroundComponent } from '../components/remove-back-ground/remove-back-ground.component';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
